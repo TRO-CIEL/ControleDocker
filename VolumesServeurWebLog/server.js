e5331d891d274dfd3bb0014237395c3d3251e08f
tpdocker <tpdocker@branly.local>
2025-12-11 09:44:28 +0100

    Commit #8 volLogWeb

--- VolumesServeurWebLog/server.js
+++ VolumesServeurWebLog/server.js
@@ -2,14 +2,13 @@
 const fs = require('fs');
 const app = express();
 const LOG_PATH = '/log/messages.log';
-
 app.use(express.static('www'));
-
 app.get('/log', (req, res) => {
   fs.readFile(LOG_PATH, 'utf8', (err, data) => {
-    if (err) return res.send('Erreur de lecture du fichier log.');
-    res.type('text/plain').send('\n' + data + '\n');
+    if (err) {
+      return res.send("Erreur de lecture du fichier log.");
+    }
+    res.send(<pre></pre>);
   });
 });
-
 app.listen(80, () => console.log('Serveur lancé sur le port 80'));
